98f326b9d44c09fa73cfeb1c72673b44815c9cec
Parveen Gartan <63051520+pga043@users.noreply.github.com>
2026-02-18 16:04:30 +0100

    Add files via upload

---
+++ utilities/get_gpu_name.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def get_gpu():
+	hostname = subprocess.check_output("hostname", shell=True).decode('ascii').split('\n')[0]
+	try:
+		gpu_info = subprocess.check_output("lspci | grep -i nvidia | grep -i vga", shell=True)
+		#gpu_lines = [line for line in gpu_info.decode("ascii").split('\n') if line.strip()]
+		#num_gpus = len(gpu_lines)
+		num_gpus = len(gpu_info.decode('utf-8').splitlines())
+		print(f"Number of GPUs: {num_gpus} on computer: {hostname}")
+		gpu_name = gpu_info.decode("ascii").split('[')[1].split(']')[0]
+		print(gpu_name)
+		return gpu_name
+	except (subprocess.CalledProcessError, FileNotFoundError, UnicodeDecodeError):
+		print(f'No GPUs detected on computer: {hostname}')
+
+get_gpu()
