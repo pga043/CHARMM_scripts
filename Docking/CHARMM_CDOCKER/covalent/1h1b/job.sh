835f04f2b6e0d7ba7ed28570afba08c4cfe6a015
Parveen Gartan <63051520+pga043@users.noreply.github.com>
2026-04-15 14:28:00 +0200

    Delete Docking/CHARMM_CDOCKER/covalent/1h1b directory

--- Docking/CHARMM_CDOCKER/covalent/1h1b/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-export charmm=/net/orinoco/pga043/CHARMM_47a2/charmm_47a2/build_openmm_blade/charmm 
-
-#$charmm -i write_pdb.inp > write_pdb.out
-
-$charmm -i gridcenter.inp > gridcenter.out
